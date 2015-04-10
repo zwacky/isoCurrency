ffcd7e1cc416942ae999d2ee9f1ac82984e94b3a
Aleksander Herforth Rendtslev <aleksanderhr@live.com>
2015-04-10 14:34:17 +0200

    implmented IsoCurrencySymbol filter

--- src/components/isoCurrency.filter.js
+++ src/components/isoCurrency.filter.js
@@ -3,8 +3,8 @@
 /**
  * wraps angular's currency filter with an additional layer, in case the currency symbol is not available.
  */
-angular.module('isoCurrency', ['isoCurrency.common'])
-	.filter('isoCurrency', ['$filter', 'iso4217', function($filter, iso4217) {
+angular.module('isoCurrency', ['isoCurrency.common', 'isoCurrency.symbol'])
+	.filter('isoCurrency', ['$filter', 'iso4217', '$log', function($filter, iso4217, $log) {
 
 		/**
 		 * transforms an amount into the right format and currency according to a passed currency code (3 chars).
@@ -18,7 +18,17 @@ angular.module('isoCurrency', ['isoCurrency.common'])
 			if (!currencyCode) {
 				return;
 			}
+			/**
+			 * Get the currency for currencyCode from the iso4217 service
+			 * @type {object}
+			 */
 			var currency = iso4217.getCurrencyByCode(currencyCode);
+
+			/**
+			 * If the given currencyCode doesn't translate into a valid currency a warning will be posted to the console.			
+			 */
+			if(typeof currency === 'undefined'){$log.warn('The provided currencyCode \"' + currencyCode + '\" did not transalte into a valid currency'); return;}
+
 			var fractionSize = (fraction === void 0) ? currency.fraction : fraction;
 			return $filter('currency')(amount, currency.symbol, fractionSize);
 		};
