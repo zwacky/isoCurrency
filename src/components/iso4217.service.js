b7771a4cbc11731c65637320bf0f047225d9c40a
Simon Wicki <simon.wicki@justwatch.com>
2015-09-19 16:46:09 +0200

    fix: added ZWL and XOF symbols

--- src/components/iso4217.service.js
+++ src/components/iso4217.service.js
@@ -100,7 +100,7 @@ angular.module('isoCurrency.common', [])
 			'XOF': {
 				text: 'CF Franc BCEAO',
 				fraction: 0,
-				symbol: false
+				symbol: 'CFA'
 			},
 			'BMD': {
 				text: 'Bermudian Dollar',
@@ -850,7 +850,7 @@ angular.module('isoCurrency.common', [])
 			'ZWL': {
 				text: 'Zimbabwe Dollar',
 				fraction: 2,
-				symbol: false
+				symbol: '$'
 			}
 		};
 
@@ -858,7 +858,7 @@ angular.module('isoCurrency.common', [])
 
 			/**
 			 * retrieves the object holding currency, code and fraction information about a currency.
-			 * 
+			 *
 			 * @param string code
 			 * @return object
 			 */
